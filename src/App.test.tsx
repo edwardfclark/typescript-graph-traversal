b60105e154055144e26aa7a1fbdf85a338b9ff5a
Edward Clark <edward.clark@usaycompare.com>
2019-09-16 20:57:33 +0100

    Take a stab at unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -17,7 +17,14 @@ it("handles no data case", () => {
   expect(graph.find("p").text()).toEqual("No adjacency list is present.");
 });
 
-it("contains a title and a subtitle", () => {
+it("contains a title with the right text", () => {
   const graph = mount(<Graph data={graphData} />);
-  const title = graph.find("title");
+  const title = graph.find("h1");
+  expect(title.text()).toEqual("Find The Path With Dijkstra");
+});
+
+it("contails a subTitle with the right text", () => {
+  const graph = mount(<Graph data={graphData} />);
+  const subTitle = graph.find("p");
+  expect(subTitle.text()).toEqual("Select nodes by clicking them to begin...");
 });
--- src/ReactGraphChallenge/graph.errorBoundary.tsx
+++ src/ReactGraphChallenge/graph.errorBoundary.tsx
@@ -4,13 +4,16 @@ interface IErrorBoundaryState {
   hasError: boolean;
 }
 
+// Errors from react-vis-force prevent the unit tests from running.
+// This error boundary will let me test everything outside of it.
 class ErrorBoundary extends React.Component<{}, IErrorBoundaryState> {
-  constructor(props) {
+  constructor(props: any) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
+  // Exceptions have any type by default
+  static getDerivedStateFromError(error: any) {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
@@ -18,9 +21,11 @@ class ErrorBoundary extends React.Component<{}, IErrorBoundaryState> {
   render() {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <p className="err">Something went wrong.</p>;
+      return <h5 className="err">Something went wrong.</h5>;
     }
 
     return this.props.children;
   }
 }
+
+export default ErrorBoundary;
